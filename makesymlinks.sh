fc09e1a228b19fc69d7cc07b6ab4610c2d327d0f
Leandro Isotton <lisotton@cit009473-ubuntu.cit>
2015-01-23 14:45:35 -0200

    Change move command

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -20,16 +20,16 @@ fi
 # create symlink to files
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $olddir"
-  mv -r ~/.$file $olddir
+  mv ~/.$file $olddir
 
   echo "Creating symlink to $file in home directory."
   ln -s $dir/.$file ~/.$file
 done
 
 echo "Creating symlink to Sublime user settings."
-mv -r ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings $olddir
+mv ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings $olddir
 ln -s $dir/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 
 echo "Creating symlink to custom oh-my-zsh plugins and themes."
-mv -r ~/.oh-my-zsh/custom $olddir
+mv ~/.oh-my-zsh/custom $olddir
 ln -s $dir/custom ~/.oh-my-zsh/custom
